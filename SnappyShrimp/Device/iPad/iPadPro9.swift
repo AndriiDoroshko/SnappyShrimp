e204003657f880f8486e701df7c59ef2a296d7db
RomanTysiachnik <pot9ka@gmail.com>
2017-12-28 18:29:21 +0200

    Fixed iPad presentations

--- SnappyShrimp/Device/iPad/iPadPro9.swift
+++ SnappyShrimp/Device/iPad/iPadPro9.swift
@@ -7,24 +7,24 @@ extension Device{
     public enum iPadPro9 {
         public enum portrait {
             public static let fullScreen = Presentation(
-                name: "iPad Pro10 FullScreen",
-                size: CGSize.iPadPro10,
+                name: "iPad Pro9 FullScreen",
+                size: CGSize.iPadPro9,
                 traitCollection: UITraitCollection(traitsFrom: [
                     UITraitCollection.iPad.portrait.fullScreen,
                     UITraitCollection.Compability.ForceTouch.available,
                     UITraitCollection.Compability.DisplayGamut.P3])
             )
             public static let twoThirds = Presentation(
-                name: "iPad Pro10 Portrait SplitView Two-Thirds",
-                size: CGSize.iPadPro10.splitViewTwoThirds,
+                name: "iPad Pro9 Portrait SplitView Two-Thirds",
+                size: CGSize.iPadPro9.splitViewTwoThirds,
                 traitCollection: UITraitCollection(traitsFrom: [
                     UITraitCollection.iPad.portrait.splitTwoThirds,
                     UITraitCollection.Compability.ForceTouch.available,
                     UITraitCollection.Compability.DisplayGamut.P3])
             )
             public static let oneThird = Presentation(
-                name: "iPad Pro10 Portrait SplitView One Third",
-                size: CGSize.iPadPro10.splitViewOneThird,
+                name: "iPad Pro9 Portrait SplitView One Third",
+                size: CGSize.iPadPro9.splitViewOneThird,
                 traitCollection: UITraitCollection(traitsFrom: [
                     UITraitCollection.iPad.portrait.splitOneThird,
                     UITraitCollection.Compability.ForceTouch.available,
@@ -33,32 +33,32 @@ extension Device{
         }
         public enum landscape {
             public static let fullScreen = Presentation(
-                name: "iPad Pro10 Landscape FullScreen",
-                size: CGSize.iPadPro10.rotated,
+                name: "iPad Pro9 Landscape FullScreen",
+                size: CGSize.iPadPro9.rotated,
                 traitCollection: UITraitCollection(traitsFrom: [
                     UITraitCollection.iPad.landscape.fullScreen,
                     UITraitCollection.Compability.ForceTouch.available,
                     UITraitCollection.Compability.DisplayGamut.P3])
             )
             public static let half = Presentation(
-                name: "iPad Pro10 Landscape SplitView Half",
-                size: CGSize.iPadPro10.rotated.splitViewHalf,
+                name: "iPad Pro9 Landscape SplitView Half",
+                size: CGSize.iPadPro9.rotated.splitViewHalf,
                 traitCollection: UITraitCollection(traitsFrom: [
                     UITraitCollection.iPad.landscape.splitHalf,
                     UITraitCollection.Compability.ForceTouch.available,
                     UITraitCollection.Compability.DisplayGamut.P3])
             )
             public static let twoThird = Presentation(
-                name: "iPad Pro10 Landscape SplitView Two-Thirds",
-                size: CGSize.iPadPro10.rotated.splitViewTwoThirds,
+                name: "iPad Pro9 Landscape SplitView Two-Thirds",
+                size: CGSize.iPadPro9.rotated.splitViewTwoThirds,
                 traitCollection: UITraitCollection(traitsFrom: [
                     UITraitCollection.iPad.landscape.splitTwoThirds,
                     UITraitCollection.Compability.ForceTouch.available,
                     UITraitCollection.Compability.DisplayGamut.P3])
             )
             public static let oneThird = Presentation(
-                name: "iPad Pro10 Landscape SplitView One Third",
-                size: CGSize.iPadPro10.rotated.splitViewOneThird,
+                name: "iPad Pro9 Landscape SplitView One Third",
+                size: CGSize.iPadPro9.rotated.splitViewOneThird,
                 traitCollection: UITraitCollection(traitsFrom: [
                     UITraitCollection.iPad.landscape.splitOneThird,
                     UITraitCollection.Compability.ForceTouch.available,
